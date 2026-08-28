9e7ded698af885ca6ce0b0fcfa7f5ebdb7439a78
Subhan <subhanulhaq935@gmail.com>
2026-08-28 11:43:30 +0500

    Add project member repository

---
+++ app/repositories/project_member_repository.py
@@ -0,0 +1,51 @@
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from app.models.project_member import ProjectMember
+
+
+def get_members_by_project(db: Session, project_id: int):
+    statement = select(ProjectMember).where(
+        ProjectMember.project_id == project_id
+    )
+
+    result = db.execute(statement)
+
+    return result.scalars().all()
+
+
+def get_project_member(
+    db: Session,
+    project_id: int,
+    user_id: int
+):
+    statement = select(ProjectMember).where(
+        ProjectMember.project_id == project_id,
+        ProjectMember.user_id == user_id
+    )
+
+    result = db.execute(statement)
+
+    return result.scalars().one_or_none()
+
+
+def create_project_member(
+    db: Session,
+    project_member: ProjectMember
+):
+    db.add(project_member)
+
+    db.commit()
+
+    db.refresh(project_member)
+
+    return project_member
+
+
+def delete_project_member(
+    db: Session,
+    project_member: ProjectMember
+):
+    db.delete(project_member)
+
+    db.commit()

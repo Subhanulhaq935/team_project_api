08e79b56a63588edacc7a3d83ba0910584e81ef7
Subhan <subhanulhaq935@gmail.com>
2026-08-28 12:10:47 +0500

    Add project member service and user repository

---
+++ app/repositories/user_repository.py
@@ -0,0 +1,12 @@
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from app.models.user import User
+
+
+def get_user_by_id(db: Session, user_id: int):
+    statement = select(User).where(User.id == user_id)
+
+    result = db.execute(statement)
+
+    return result.scalars().one_or_none()
